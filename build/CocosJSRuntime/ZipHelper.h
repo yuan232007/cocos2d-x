7c730bdf069e5c1d8ab50b12f219aecc7e081121
Wenhai Lin <wenhai.lin@chukong-inc.com>
2015-10-16 18:12:34 +0800

    添加解压zip包到指定目录的功能

---
+++ build/CocosJSRuntime/ZipHelper.h
@@ -0,0 +1,41 @@
+#ifndef _RUNTIME_ZIP_HELPER_H
+#define _RUNTIME_ZIP_HELPER_H
+
+#import <Foundation/Foundation.h>
+
+@protocol ZipArchiveDelegate;
+
+@interface ZipHelper: NSObject
+
+// Unzip
++ (BOOL)unzipFileAtPath:(NSString *)path
+          toDestination:(NSString *)destination;
+
++ (BOOL)unzipFileAtPath:(NSString *)path
+          toDestination:(NSString *)destination
+              overwrite:(BOOL)overwrite
+               password:(NSString *)password
+                  error:(NSError *)error;
+
+// Zip
++ (BOOL)createZipFileAtPath:(NSString *)path
+           withFilesAtPaths:(NSArray *)paths;
+
++ (BOOL)createZipFileAtPath:(NSString *)path
+    withContentsOfDirectory:(NSString *)directoryPath;
+
++ (BOOL)createZipFileAtPath:(NSString *)path
+    withContentsOfDirectory:(NSString *)directoryPath
+        keepParentDirectory:(BOOL)keepParentDirectory;
+
+- (instancetype)initWithPath:(NSString *)path NS_DESIGNATED_INITIALIZER;
+
+@property (NS_NONATOMIC_IOSONLY, readonly) BOOL open;
+@property (NS_NONATOMIC_IOSONLY, readonly) BOOL close;
+
+@end
+
+
+
+
+#endif /* _RUNTIME_ZIP_HELPER_H */
